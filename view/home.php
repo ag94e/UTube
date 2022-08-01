eebd1fba6530e2071e8608557efe021df0fba8dd
Antonio Guerrero <antonio.2.guerrero@atos.net>
2022-07-31 19:34:49 -0500

    adding missing pages

---
+++ view/home.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@300;600&family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Home</title>
+</head>
+<body>
+<nav class="navbar fixed-top bg-dark">
+    <div class="container-fluid">
+        <a href="#" class="UTcolor navbar-brand d-flex align-items-center">
+        <img src="../assets/UTubeLogo.png" width="35" height="35" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></img>
+        <strong>UTube</strong>
+        </a>
+        <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+
+<section style="padding-top: 5em;">
+    <div class="row">
+        <div class="offset-3 col-6">
+            <div class="col-12">
+                <div class="card shadow-sm">
+                    <video src="Desarrollo Movil.mp4" controls autoplay></video> 
+                    <div class="card-body">
+                    <h4 class="card-text">Desarrollo Movil</h4>
+                    <p>Lenguajes para desarrollo Movil</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Descargar</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Compartir</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="col-12">
+                <div class="card shadow-sm">
+                    <video src="Desarrollo Movil.mp4" controls autoplay></video> 
+                    <div class="card-body">
+                    <h4 class="card-text">Desarrollo web</h4>
+                    <p>Tecnologías para el desarrollo web</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Descargar</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Compartir</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
--- view/profile.php
+++ view/profile.php
@@ -13,7 +13,7 @@
   </head>
   <body>
   <header>
-  <nav class="navbar bg-dark">
+  <nav class="navbar fixed-top bg-dark">
     <div class="container-fluid">
       <a href="#" class="UTcolor navbar-brand d-flex align-items-center">
         <img src="../assets/UTubeLogo.png" width="35" height="35" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></img>
